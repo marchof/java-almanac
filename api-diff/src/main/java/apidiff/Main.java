7c652904fc7f3dfc278e813c37e4df70f1a9cf08
Marc R. Hoffmann <hoffmann@mountainminds.com>
2018-10-03 20:04:04 +0200

    Generate diffs between all known JDKs.

--- api-diff/src/main/java/apidiff/Main.java
+++ api-diff/src/main/java/apidiff/Main.java
@@ -3,6 +3,8 @@ package apidiff;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 import apidiff.cmp.Delta;
 import apidiff.loader.Loader;
@@ -12,6 +14,8 @@ import apidiff.report.FileMultiReportOutput;
 import apidiff.report.HTMLRenderer;
 
 public class Main {
+	
+	private static final Map<JDK, ApiInfo> cache = new HashMap<>();
 
 	private static ApiInfo loadVersion(JDK jdk) throws IOException {
 		ApiInfo api = new ApiInfo(jdk.getName(), jdk.getImpl());
@@ -19,20 +23,31 @@ public class Main {
 		loader.loadJDK(Paths.get(System.getProperty("user.home"), ".sdkman/candidates/java", jdk.getImpl()));
 		return api;
 	}
+	
+	private static ApiInfo getVersion(JDK jdk) throws IOException {
+		ApiInfo api = cache.get(jdk);
+		if (api == null) {
+			api = loadVersion(jdk);
+			cache.put(jdk, api);
+		}
+		return api;
+	}
 
 	private static void createReport(JDK a, JDK b) throws IOException {
-		ApiInfo api1 = loadVersion(a);
-		ApiInfo api2 = loadVersion(b);
+		ApiInfo api1 = getVersion(a);
+		ApiInfo api2 = getVersion(b);
 		Delta delta = new Delta(api1, api2);
 		Path basedir = Paths.get("./target/diff").resolve(a.name()).resolve(b.name());
 		new HTMLRenderer(new FileMultiReportOutput(basedir), b.getDoc()).render(delta);
 	}
 
 	public static void main(String[] args) throws IOException {
-		createReport(JDK.V8, JDK.V9);
-		createReport(JDK.V9, JDK.V10);
-		createReport(JDK.V10, JDK.V11);
-		createReport(JDK.V11, JDK.V12);
+		JDK[] jdks = JDK.values();
+		for (int i = 0; i < jdks.length; i++) {
+			for (int j = i + 1; j < jdks.length; j++) {
+				createReport(jdks[i], jdks[j]);
+			}
+		}
 	}
 
 }
