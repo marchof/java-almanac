74a42b9be4cc7f93280eadc4566c906124a8dfbe
Marc R. Hoffmann <hoffmann@mountainminds.com>
2019-01-22 22:02:27 +0100

    Consider latest JDK updates.

--- api-diff/src/main/java/apidiff/JDK.java
+++ api-diff/src/main/java/apidiff/JDK.java
@@ -8,15 +8,15 @@ import apidiff.javadoc.NoJavaDoc;
 
 public enum JDK {
 
-	V8("Java 8", "8.0.191-oracle", new NoJavaDoc()),
+	V8("Java 8", "8.0.201-oracle", new NoJavaDoc()),
 	
 	V9("Java 9", "9.0.4-open", new JavaDoc9("https://docs.oracle.com/javase/9/docs/api/")),
 	
 	V10("Java 10", "10.0.2-open", new JavaDoc10("https://docs.oracle.com/javase/10/docs/api/")),
 	
-	V11("Java 11", "11.0.1-open", new JavaDoc11("https://docs.oracle.com/en/java/javase/11/docs/api/")),
+	V11("Java 11", "11.0.2-open", new JavaDoc11("https://docs.oracle.com/en/java/javase/11/docs/api/")),
 	
-	V12("Java 12", "12.ea.20-open", new JavaDoc11("https://download.java.net/java/early_access/jdk12/docs/api/"));
+	V12("Java 12", "12.ea.26-open", new JavaDoc11("https://download.java.net/java/early_access/jdk12/docs/api/"));
 
 	private String name;
 	private String impl;
