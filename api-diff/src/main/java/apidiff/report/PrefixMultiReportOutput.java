8bdffff88d9119efecef85c9a923729012167527
Marc R. Hoffmann <hoffmann@mountainminds.com>
2019-06-21 17:23:46 +0200

    Add direct S3 output

---
+++ api-diff/src/main/java/apidiff/report/PrefixMultiReportOutput.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2019 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marc R. Hoffmann - initial API and implementation
+ *    
+ *******************************************************************************/
+package apidiff.report;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Implementation of {@link IMultiReportOutput} that prefixes the output path.
+ */
+public class PrefixMultiReportOutput implements IMultiReportOutput {
+
+	private IMultiReportOutput delegate;
+	private String prefix;
+
+	public PrefixMultiReportOutput(IMultiReportOutput delegate, String prefix) {
+		this.delegate = delegate;
+		this.prefix = prefix;
+	}
+
+	public OutputStream createFile(final String path) throws IOException {
+		return delegate.createFile(prefix + path);
+	}
+
+	public void close() throws IOException {
+		delegate.close();
+	}
+
+}
