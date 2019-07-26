4e0af98b76287cd1e6194899397c862f55fc7b7a
Marc R. Hoffmann <hoffmann@mountainminds.com>
2019-07-26 15:14:36 +0200

    Use AdoptOpenJDK for LTS releases

--- api-diff/src/main/java/apidiff/JDK.java
+++ api-diff/src/main/java/apidiff/JDK.java
@@ -8,13 +8,13 @@ import apidiff.javadoc.NoJavaDoc;
 
 public enum JDK {
 
-	V8("Java 8", "8.0.212.hs-adpt", new NoJavaDoc()),
+	V8("Java 8", "8.0.222.hs-adpt", new NoJavaDoc()),
 
 	V9("Java 9", "9.0.4-open", new JavaDoc9("https://docs.oracle.com/javase/9/docs/api/")),
 
 	V10("Java 10", "10.0.2-open", new JavaDoc10("https://docs.oracle.com/javase/10/docs/api/")),
 
-	V11("Java 11", "11.0.2-open", new JavaDoc11("https://docs.oracle.com/en/java/javase/11/docs/api/")),
+	V11("Java 11", "11.0.4.hs-adpt", new JavaDoc11("https://docs.oracle.com/en/java/javase/11/docs/api/")),
 
 	V12("Java 12", "12.0.2-open", new JavaDoc11("https://docs.oracle.com/en/java/javase/12/docs/api/")),
 
