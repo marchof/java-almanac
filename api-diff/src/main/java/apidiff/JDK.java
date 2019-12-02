6c7d31a2405702a19f4eb5282ef1274acf20fd3c
Marc R. Hoffmann <hoffmann@mountainminds.com>
2019-12-02 08:14:39 +0100

    Update to latest build of Java 14

--- api-diff/src/main/java/apidiff/JDK.java
+++ api-diff/src/main/java/apidiff/JDK.java
@@ -20,7 +20,7 @@ public enum JDK {
 
 	V13("Java 13", "13.0.1-open", new JavaDoc11("https://docs.oracle.com/en/java/javase/13/docs/api/")),
 
-	V14("Java 14", "14.ea.24-open", new JavaDoc11("https://download.java.net/java/early_access/jdk14/docs/api/"));
+	V14("Java 14", "14.ea.25-open", new JavaDoc11("https://download.java.net/java/early_access/jdk14/docs/api/"));
 
 	private String name;
 	private String impl;
