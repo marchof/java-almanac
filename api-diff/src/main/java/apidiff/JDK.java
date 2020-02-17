c49e4ea49513fe8261654a11a0b29a0cc73af211
Marc R. Hoffmann <hoffmann@mountainminds.com>
2020-02-17 06:33:19 +0100

    Update to latest builds of Java 14 and 15

--- api-diff/src/main/java/apidiff/JDK.java
+++ api-diff/src/main/java/apidiff/JDK.java
@@ -36,9 +36,9 @@ public enum JDK {
 
 	V13("13", "13.0.2.hs-adpt", new JavaDoc11("https://docs.oracle.com/en/java/javase/13/docs/api/")),
 
-	V14("14", "14.ea.34-open", new JavaDoc11("https://download.java.net/java/early_access/jdk14/docs/api/")),
+	V14("14", "14.ea.36-open", new JavaDoc11("https://download.java.net/java/early_access/jdk14/docs/api/")),
 
-	V15("15", "15.ea.8-open", new JavaDoc11("https://download.java.net/java/early_access/jdk15/docs/api/"));
+	V15("15", "15.ea.10-open", new JavaDoc11("https://download.java.net/java/early_access/jdk15/docs/api/"));
 
 	private String name;
 	private String impl;
