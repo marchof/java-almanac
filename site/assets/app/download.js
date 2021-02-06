280b975b03d3f17576956bae091f911307dd9476
Marc R. Hoffmann <hoffmann@mountainminds.com>
2021-02-06 14:46:28 +0100

    Second lookup to refresh ephemeral_id when download is requested

--- site/assets/app/download.js
+++ site/assets/app/download.js
@@ -17,7 +17,7 @@ Vue.component('downloadlist', {
               <td>{{ package.java_version }}</td>
               <td>{{ package.package_type }}</td>
               <td>{{ package.operating_system }}-{{ package.architecture }}</td>
-              <td><a href="" @click.prevent="download(package.ephemeral_id)">{{ package.filename }}</a></td>
+              <td><a href="" @click.prevent="download(package.id)">{{ package.filename }}</a></td>
             </tr>
           </tbody>
         </table>
@@ -29,18 +29,21 @@ Vue.component('downloadlist', {
         };
     },
     mounted() {
-        url = "https://api.foojay.io/disco/v1.0/packages" + window.location.search;
-        axios.get(url).then(response => { 
+        this.discoRequest("packages/" + window.location.search, response => { 
             this.packages = response.data;
         });
     },
     methods: {
-        download(ephermalid) {
-            url = "https://api.foojay.io/disco/v1.0/ephemeral_ids/" + ephermalid;
-            axios.get(url).then(response => {
-                link = response.data.direct_download_uri || response.data.download_site_uri;
-                window.open(link, "_self");
+        download(packageid) {
+            this.discoRequest("packages/" + packageid, response => {
+                this.discoRequest("ephemeral_ids/" + response.data.ephemeral_id, response => {
+                    link = response.data.direct_download_uri || response.data.download_site_uri;
+                    window.open(link, "_self");
+                });
             });
+        },
+        discoRequest(path, handler) {
+            axios.get("https://api.foojay.io/disco/v1.0/" + path).then(handler);
         }
     }
 });
